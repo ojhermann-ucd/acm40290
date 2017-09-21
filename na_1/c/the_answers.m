bd7c19403f5f0327f73a5dabe632851e101433ec
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-09-21 17:22:29 +0100

    taking a break for a climb

--- na_1/c/the_answers.m
+++ na_1/c/the_answers.m
@@ -1,9 +1,9 @@
-disp(EulerN(power(10, 1)))
-disp(EulerN(power(10, 3)))
-disp(EulerN(power(10, 6)))
-disp(EulerN(power(10, 9)))
+n = [1 3 6 9];
+for i = n(1:4);
+	disp(EulerN(power(10, i)))
+end
 
 % 2.7182818011463845131459038384492
 % 2.7182818284590455348848081484903
-% 
+% 2.7182818284590455348848081484903
 % 
--- na_1/d/the_answers.m
+++ na_1/d/the_answers.m
@@ -1,7 +1,7 @@
-disp(EulerC(power(10, 1)))
-disp(EulerC(power(10, 3)))
-disp(EulerC(power(10, 6)))
-disp(EulerC(power(10, 9)))
+n = [1 3 6 9];
+for i = n(1:4);
+	disp(EulerC(power(10, i)))
+end
 
 % 0.62638316097420787542660036706366
 % 0.57771558156820645990592311136425
