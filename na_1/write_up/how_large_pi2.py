4a7c370b73a00bd8060ce26688201a63472d3248
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-09-24 23:16:28 +0100

    pi2 ready

--- na_1/write_up/how_large_pi2.py
+++ na_1/write_up/how_large_pi2.py
@@ -1,3 +1,5 @@
+# Pi2(n)
+
 def product_element_1(n):
 	return (2*n) / (2*n - 1)
 
@@ -11,19 +13,12 @@ def approximation_of_pi():
 	n = 1
 	prior = 1
 	current = 1
-	change = True
-	while change:
+	over_flow = False
+	while not over_flow:
 		prior = current
-		current *= (2 * product_element(n))
+		current *= (product_element(n))
 		n += 1
-		change = (current != prior)
-		print("Just tried {}".format(n - 1))
-		print(current)
-	return format(n - 1, ".32f")
+		over_flow = (current*2 == float("Inf"))
+	return [n - 1, format(prior*2, ".32f")]
 
-# print(approximation_of_pi())
-
-if 3 == float("Inf"):
-	print("y")
-else:
-	print("n")
+print(approximation_of_pi())
