b7ae118db6200fe6c4e5e11d7aa4e11b18ad7a4d
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-10-08 15:33:17 +0100

    completed MachEps and write up

--- a2_floating_point_number_systems/MachEps.m
+++ a2_floating_point_number_systems/MachEps.m
@@ -1,17 +1,23 @@
-function value = MachEps()
+function [me_estimate, precision] = MachEps()
     
     % instantiate the estimate for machine epsilon
-    me = 1.0;
+    me_estimate = 1.0;
+    precision = 0;
 
     % use a while loop to refine the estimate
-    while (1.0 + me) > 1.0
-    	me = me / 2;
+    while (1.0 + me_estimate) > 1.0
+    	me_estimate = me_estimate / 2;
+    	precision = precision + 1;
     end
+
+    % return accounting for e/2 in the last step
+    me_estimate = me_estimate * 2.0;
+    precision = precision;
     
-    % set the return format
-    format long;
-
-    % return accounting for e/2
-    value = me * 2.0;
-
+    
 end
+
+%{
+    My solution used a while loop to iteratively determine machine epsilon, the difference between 1.0 and the next hightest floating point number.  If for a given value of me, Matlab was able to distinguish between 1.0 and 1.0+me, I would divide me by 2 and repeat.  Once 1.0 and 1.0+me were indistinguishable, I returned the penultimate value of me i.e. 2 * me, as it was the last value that allowed Matlab to delineate 1.0 and 1.0+me.
+    My results were identical to Matlabs eps, the built in value of me, so my method seems to work well.  Also, knowing that our base is 2 and seeing that precision is estimated to be 53, we can check that 2^-52 = out output = b^1-p as desired.
+%}
--- a2_floating_point_number_systems/MachEps.py
+++ a2_floating_point_number_systems/MachEps.py
@@ -2,12 +2,14 @@ def mach_eps():
 
 	# instantiate machine epsilon
 	me = 1.0
+	count = 0
 
 	# while loop to determine me
 	while (1.0 + me) > 1.0:
 		me /= 2
+		count += 1
 
 	# return by converting from e/2
-	return me * 2
+	return [me * 2, count - 1]
 
 print(mach_eps())
